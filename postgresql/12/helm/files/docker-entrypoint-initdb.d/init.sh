8419f9ba5e8763b6ece2a754bb6f89269b11dad8
alpanachaphalkar <alpana.chaphalkar@sky.de>
2023-02-08 08:21:21 +0100

    TIBCO OM 6.0.0 Postgres DB scripts added.

--- postgresql/12/helm/files/docker-entrypoint-initdb.d/init.sh
+++ postgresql/12/helm/files/docker-entrypoint-initdb.d/init.sh
@@ -6,22 +6,11 @@ mkdir -p "/tablespaces/admintablespace" && mkdir -p "/tablespaces/archivaltables
 cd /dbscripts
 
 cd ./admin/bin && sh ./db-setup.sh
-sh ./upgrade_5.1.0_to_5.1.0hf1_db-setup.sh
-sh ./upgrade_5.1.0hf1_to_5.1.0hf2_db-setup.sh
-sh ./upgrade_5.1.0hf5_to_5.1.0hf6_db-setup.sh
 
 cd ../../archival/bin && sh ./db-setup.sh
-sh ./upgrade_5.1.0_to_5.1.0hf1_db-setup.sh
-sh ./upgrade_5.1.0hf5_to_5.1.0hf6_db-setup.sh
-sh ./upgrade_5.1.0hf6_to_5.1.0hf7_db-setup.sh
 
 cd ../../catalog/bin && sh ./db-setup.sh
-sh ./upgrade_5.1.0hf4_to_5.1.0hf5_db-setup.sh
 
 cd ../../jeopardy/bin && sh ./db-setup.sh
-sh ./upgrade_5.1.0hf3_to_5.1.0hf4_db-setup.sh
 
 cd ../../order/bin && sh ./db-setup.sh
-sh ./upgrade_5.1.0hf1_to_5.1.0hf2_db-setup.sh
-sh ./upgrade_5.1.0hf5_to_5.1.0hf6_db-setup.sh
-sh ./upgrade_5.1.0hf6_to_5.1.0hf7_db-setup.sh
